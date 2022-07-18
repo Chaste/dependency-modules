55fb78d361de8ff58818012d98b48ab83c7dd4ac
Kwabena Amponsah <kwabenantim@yahoo.com>
2022-07-18 10:45:27 +0000

    Adding Chaste Jammy packages

--- scripts/setup_ubuntu2204.sh
+++ scripts/setup_ubuntu2204.sh
@@ -1,1 +1,22 @@
 #!/bin/bash
+
+export DEBIAN_FRONTEND=noninteractive
+
+apt-get update && \
+apt-get install -y --no-install-recommends \
+apt-utils \
+apt-transport-https \
+ca-certificates \
+gnupg \
+openssl \
+wget \
+curl \
+rsync
+
+echo "deb [signed-by=/usr/share/keyrings/chaste.asc] http://www.cs.ox.ac.uk/chaste/ubuntu jammy/" > /etc/apt/sources.list.d/chaste.list
+apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 422C4D99
+wget -O /usr/share/keyrings/chaste.asc https://www.cs.ox.ac.uk/chaste/ubuntu/Chaste%20Team.asc
+
+apt-get update && \
+apt-get install -y --no-install-recommends \
+chaste-dependencies
