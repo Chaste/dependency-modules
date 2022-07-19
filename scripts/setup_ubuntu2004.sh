0477457691ee37e6fc5088481e1f82289218f99f
Kwabena Amponsah <kwabenantim@yahoo.com>
2022-07-19 14:06:41 +0000

    Install recommended Focal chaste-deps

--- scripts/setup_ubuntu2004.sh
+++ scripts/setup_ubuntu2004.sh
@@ -9,40 +9,22 @@ apt-utils \
 apt-transport-https \
 ca-certificates \
 gnupg \
-openssl
+openssl \
+wget \
+curl \
+rsync
 
 echo "deb http://www.cs.ox.ac.uk/chaste/ubuntu focal/" > /etc/apt/sources.list.d/chaste.list
 apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 422C4D99
 
-apt-get update && \
-apt-get install -y --no-install-recommends \
-chaste-dependencies
+apt-get update && apt-get install -y chaste-dependencies
 
 apt-get install -y --no-install-recommends \
-cmake \
-cmake-curses-gui \
-scons \
 python3-dev \
-python3-venv \
 python3-pip \
-python3-setuptools \
-python2 \
 libffi-dev \
-doxygen \
-git \
-patch \
-curl \
-wget \
-rsync \
-valgrind \
-libgoogle-perftools-dev \
-libvtk7-dev \
-graphviz \
-gnuplot \
 mencoder \
 mplayer \
-"libpetsc-real*-dbg" \
-hdf5-tools \
 openssh-client \
 environment-modules
 
