d5871195d90dd6a3c363aa8c78c14810fdbac9e1
Kwabena N Amponsah <kwabenantim@yahoo.com>
2022-08-14 17:01:06 +0100

    Restructuring docker-entrypoint

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -1,9 +1,13 @@
-#!/bin/bash -eu
+#!/bin/bash -e
 
 if [ ! -f "${RUNNER_DIR}/.runner" ]; then
     config_runner.sh "$@" \
         --runner_dir="${RUNNER_DIR}" \
         --work_dir="${WORK_DIR}"
 fi
+
+. /etc/profile.d/modules.sh
+mkdir -p ${MODULES_DIR}/modulefiles
+module use ${MODULES_DIR}/modulefiles
 
 exec "${RUNNER_DIR}/run.sh"
