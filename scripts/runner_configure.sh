c7853add664eecc52aef045ab5a1283fd37ac89a
Kwabena N Amponsah <kwabenantim@yahoo.com>
2022-08-15 22:30:10 +0100

    Adding runner remove

--- scripts/runner_configure.sh
+++ scripts/runner_configure.sh
@@ -17,6 +17,32 @@ else
     unattended="--unattended"
 fi
 
+# Get runner_dir
+runner_dir="${RUNNER_DIR:-}"
+
+if [[ (-z "${runner_dir}") && (-z "${unattended}") ]]; then
+    echo -n "Enter path to actions-runner: "
+    read runner_dir
+fi
+
+if [ -z "${runner_dir}" ]; then
+    throw_error "RUNNER_DIR not specified"
+fi
+
+# Get personal access token
+pa_token="${RUNNER_PA_TOKEN-}"
+unset RUNNER_PA_TOKEN
+
+if [[ (-z "${pa_token}") && (-z "${unattended}") ]]; then
+    echo -n "Enter access token: "
+    read -s pa_token
+    echo
+fi
+
+if [ -z "${pa_token}" ]; then
+    throw_error "RUNNER_PA_TOKEN not provided"
+fi
+
 # Get scope
 scope="${RUNNER_SCOPE:-}"
 
@@ -58,34 +84,46 @@ if [ "${scope}" = "repo" ]; then
     fi
 fi
 
-# Get runner_dir
-runner_dir="${RUNNER_DIR:-}"
+# Runner removal
+remove="${RUNNER_REMOVE:-}"
 
-if [[ (-z "${runner_dir}") && (-z "${unattended}") ]]; then
-    echo -n "Enter path to actions-runner: "
-    read runner_dir
+if [ -n "${remove}" ]; then
+    # Set urls for remove api
+    rm_url=
+    if [ "${scope}" = "org" ]; then
+        rm_url="https://api.github.com/orgs/${org}/actions/runners/remove-token"
+
+    elif [ "${scope}" = "repo" ]; then
+        rm_url="https://api.github.com/repos/${org}/${repo}/actions/runners/remove-token"
+    fi
+
+    # Get remove token from api
+    rm_token="$(curl -X POST -fsS -H "Accept: application/vnd.github+json" -H "Authorization: token ${pa_token}" "${rm_url}")"
+    rm_token="$(echo ${rm_token} | jq -r .token)"
+    unset pa_token
+
+    # Remove runner
+    ${runner_dir}/config.sh remove --token "${rm_token}"
+    unset rm_token
+    exit 0
 fi
 
-if [ -z "${runner_dir}" ]; then
-    throw_error "RUNNER_DIR not specified"
+# Set urls for configuration and registration api
+cfg_url=
+reg_url=
+if [ "${scope}" = "org" ]; then
+    cfg_url="https://github.com/${org}"
+    reg_url="https://api.github.com/orgs/${org}/actions/runners/registration-token"
+
+elif [ "${scope}" = "repo" ]; then
+    cfg_url="https://github.com/${org}/${repo}"
+    reg_url="https://api.github.com/repos/${org}/${repo}/actions/runners/registration-token"
 fi
 
-# Get work_dir
-work_dir="${RUNNER_WORK_DIR:-${HOME}/_work}"
-
-# Get personal access token
-pa_token="${RUNNER_PA_TOKEN-}"
-unset RUNNER_PA_TOKEN
-
-if [[ (-z "${pa_token}") && (-z "${unattended}") ]]; then
-    echo -n "Enter access token: "
-    read -s pa_token
-    echo
-fi
-
-if [ -z "${pa_token}" ]; then
-    throw_error "RUNNER_PA_TOKEN not provided"
-fi
+# Get registration token from api
+reg_token="$(curl -X POST -fsS -H "Accept: application/vnd.github+json" -H "Authorization: token ${pa_token}" "${reg_url}")"
+reg_token="$(echo ${reg_token} | jq -r .token)"
+unset pa_token
 
 # Get name, labels, group
 name="${RUNNER_NAME:-}"
@@ -99,26 +137,8 @@ if [ -n "${unattended}" ]; then
     group="${group:-Default}"
 fi
 
-# Set urls for config and api
-cfg_url=
-api_url=
-if [ "${scope}" = "org" ]; then
-    cfg_url="https://github.com/${org}"
-    api_url="https://api.github.com/orgs/${org}/actions/runners/registration-token"
-
-elif [ "${scope}" = "repo" ]; then
-    cfg_url="https://github.com/${org}/${repo}"
-    api_url="https://api.github.com/repos/${org}/${repo}/actions/runners/registration-token"
-fi
-
-# Get registration token from api
-reg_token="$(curl -X POST -fsS \
--H "Accept: application/vnd.github+json" \
--H "Authorization: token ${pa_token}" \
-"${api_url}")"
-reg_token="$(echo ${reg_token} | jq -r .token)"
-
-unset pa_token
+# Get work_dir
+work_dir="${RUNNER_WORK_DIR:-${HOME}/_work}"
 
 # Configure actions-runner
 mkdir -p ${work_dir}
