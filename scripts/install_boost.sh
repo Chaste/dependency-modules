15ad797e3899bc98b4c6a3c4a18aeeccf53f29f4
Kwabena N Amponsah <kwabenantim@yahoo.com>
2022-08-19 19:09:50 +0100

    Fixing tar for boost

--- scripts/install_boost.sh
+++ scripts/install_boost.sh
@@ -57,7 +57,7 @@ if [[ (${major} -eq 1) && (${minor} -eq 62) ]]; then  # Boost == 1.62.x
 else  # Boost > 1.62.x
     wget -nc https://boostorg.jfrog.io/artifactory/main/release/${version}/source/boost_${ver_si_on}.tar.bz2
 fi
-# tar -xjf boost_${ver_si_on}.tar.bz2
+tar -xjf boost_${ver_si_on}.tar.bz2
 
 src_dir="$(pwd)/boost_${ver_si_on}"
 
