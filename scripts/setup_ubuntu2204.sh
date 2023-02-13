c47758fe73c775755508bf7de8b637e71ed5f27b
Kwabena N Amponsah <kwabenantim@yahoo.com>
2023-02-13 22:01:11 +0000

    Update jammy setup for libunwind

--- scripts/setup_ubuntu2204.sh
+++ scripts/setup_ubuntu2204.sh
@@ -21,6 +21,7 @@ apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 422C4D99
 wget -O /usr/share/keyrings/chaste.asc https://www.cs.ox.ac.uk/chaste/ubuntu/Chaste%20Team.asc
 
 apt-get update && \
+apt-get remove -y libunwind* && \
 apt-get install -y --no-install-recommends \
     chaste-dependencies \
     git \
