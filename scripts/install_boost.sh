13830caec82d0c156f79420b75ef2fdfd0bad1de
Kwabena N Amponsah <kwabenantim@yahoo.com>
2023-02-14 00:28:46 +0000

    Patch boost _Py_fopen bug

--- scripts/install_boost.sh
+++ scripts/install_boost.sh
@@ -61,11 +61,11 @@ tar -xjf boost_${ver_si_on}.tar.bz2
 
 src_dir="$(pwd)/boost_${ver_si_on}"
 
-# Patch for Python 3.10+ in Boost <= 1.74.x
+# Fix for Python 3.10+ in Boost <= 1.74.x
 # https://github.com/boostorg/python/commit/cbd2d9f033c61d29d0a1df14951f4ec91e7d05cd
 if [[ (${major} -eq 1) && (${minor} -le 74) ]]; then  # Boost <= 1.74.x
-    cd ${src_dir}/libs/python
-    patch -t -p1 < ${script_dir}/patches/boost_174-python310-fopen.patch
+    cd ${src_dir}/libs/python/src
+    sed -i.bak 's#_Py_fopen#fopen#g' exec.cpp
 fi
 
 # Patch for Python 3.7+ in Boost <= 1.66.x
