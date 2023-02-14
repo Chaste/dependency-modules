c10a1a95e3c058aba1f154b0f386aaee6d591561
Kwabena N Amponsah <kwabenantim@yahoo.com>
2023-02-14 17:30:46 +0000

    vtk debian patches

---
+++ scripts/patches/vtk6_combinediff.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# https://sources.debian.org/patches/vtk6/6.3.0%2Bdfsg2-8.1/
+
+wget https://sources.debian.org/data/main/v/vtk6/6.3.0%2Bdfsg2-8.1/debian/patches/10_allpatches.patch
+wget https://sources.debian.org/data/main/v/vtk6/6.3.0%2Bdfsg2-8.1/debian/patches/20_soversion-sharedlib.patch
+wget https://sources.debian.org/data/main/v/vtk6/6.3.0%2Bdfsg2-8.1/debian/patches/30_matplotlib.patch
+wget https://sources.debian.org/data/main/v/vtk6/6.3.0%2Bdfsg2-8.1/debian/patches/40_use_system_sqlite.patch
+wget https://sources.debian.org/data/main/v/vtk6/6.3.0%2Bdfsg2-8.1/debian/patches/50_use_system_utf8.patch
+wget https://sources.debian.org/data/main/v/vtk6/6.3.0%2Bdfsg2-8.1/debian/patches/60_use_system_mpi4py.patch
+wget https://sources.debian.org/data/main/v/vtk6/6.3.0%2Bdfsg2-8.1/debian/patches/70_fix_ftbfs_gcc49.patch
+wget https://sources.debian.org/data/main/v/vtk6/6.3.0%2Bdfsg2-8.1/debian/patches/80_fix_arm_compilation.patch
+wget https://sources.debian.org/data/main/v/vtk6/6.3.0%2Bdfsg2-8.1/debian/patches/90_gdal-2.0.patch
+wget https://sources.debian.org/data/main/v/vtk6/6.3.0%2Bdfsg2-8.1/debian/patches/95_ffmpeg_2.9.patch
+wget https://sources.debian.org/data/main/v/vtk6/6.3.0%2Bdfsg2-8.1/debian/patches/97_fix_latex_doxygen.patch
+wget https://sources.debian.org/data/main/v/vtk6/6.3.0%2Bdfsg2-8.1/debian/patches/100_javac-heap.patch
+wget https://sources.debian.org/data/main/v/vtk6/6.3.0%2Bdfsg2-8.1/debian/patches/101_java_install_path.patch
+wget https://sources.debian.org/data/main/v/vtk6/6.3.0%2Bdfsg2-8.1/debian/patches/104_fix_gcc_version_6.patch
+wget https://sources.debian.org/data/main/v/vtk6/6.3.0%2Bdfsg2-8.1/debian/patches/105_unforce_embedded_glew.patch
+wget https://sources.debian.org/data/main/v/vtk6/6.3.0%2Bdfsg2-8.1/debian/patches/106_install_doxygen_scripts_in_nodoc_build.patch
+wget https://sources.debian.org/data/main/v/vtk6/6.3.0%2Bdfsg2-8.1/debian/patches/99-hdf5-1.10-compatibility
+wget https://sources.debian.org/data/main/v/vtk6/6.3.0%2Bdfsg2-8.1/debian/patches/108_Doxygen-use-mathjax.patch
+wget https://sources.debian.org/data/main/v/vtk6/6.3.0%2Bdfsg2-8.1/debian/patches/109_infovis_boost.patch
+wget https://sources.debian.org/data/main/v/vtk6/6.3.0%2Bdfsg2-8.1/debian/patches/110_remove_nonfree_from_build.patch
+wget https://sources.debian.org/data/main/v/vtk6/6.3.0%2Bdfsg2-8.1/debian/patches/120_fix_ftbfs_qtpainter.patch
+wget https://sources.debian.org/data/main/v/vtk6/6.3.0%2Bdfsg2-8.1/debian/patches/3edc0de2b04ae1e100c229e592d6b9fa94f2915a.patch
+wget https://sources.debian.org/data/main/v/vtk6/6.3.0%2Bdfsg2-8.1/debian/patches/581d9eb874b2b80a3fb21c739a96fa6f955ffb5e.patch
+wget https://sources.debian.org/data/main/v/vtk6/6.3.0%2Bdfsg2-8.1/debian/patches/new-freetype.patch
+
+combinediff 10_allpatches.patch 20_soversion-sharedlib.patch > tmp1.patch
+combinediff tmp1.patch 30_matplotlib.patch > tmp2.patch
+combinediff tmp2.patch 40_use_system_sqlite.patch > tmp1.patch 
+combinediff tmp1.patch 60_use_system_mpi4py.patch > tmp2.patch
+combinediff tmp2.patch 70_fix_ftbfs_gcc49.patch > tmp1.patch
+combinediff tmp1.patch 80_fix_arm_compilation.patch > tmp2.patch
+combinediff tmp2.patch 90_gdal-2.0.patch > tmp1.patch
+combinediff tmp1.patch 95_ffmpeg_2.9.patch > tmp2.patch
+combinediff tmp2.patch 97_fix_latex_doxygen.patch > tmp1.patch
+combinediff tmp1.patch 100_javac-heap.patch > tmp2.patch 
+combinediff tmp2.patch 101_java_install_path.patch > tmp1.patch 
+combinediff tmp1.patch 105_unforce_embedded_glew.patch > tmp2.patch 
+combinediff tmp2.patch 106_install_doxygen_scripts_in_nodoc_build.patch > tmp1.patch 
+combinediff tmp1.patch 99-hdf5-1.10-compatibility > tmp2.patch 
+combinediff tmp2.patch 108_Doxygen-use-mathjax.patch > tmp1.patch 
+combinediff tmp1.patch 109_infovis_boost.patch > tmp2.patch 
+combinediff tmp2.patch 110_remove_nonfree_from_build.patch > tmp1.patch 
+combinediff tmp1.patch 120_fix_ftbfs_qtpainter.patch > tmp2.patch 
+combinediff tmp2.patch 3edc0de2b04ae1e100c229e592d6b9fa94f2915a.patch > tmp1.patch 
+combinediff tmp1.patch 581d9eb874b2b80a3fb21c739a96fa6f955ffb5e.patch > tmp2.patch 
+combinediff tmp2.patch new-freetype.patch > vtk6.patch 
+rm tmp1.patch tmp2.patch
+
---
+++ scripts/patches/vtk7_combinediff.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# https://sources.debian.org/patches/vtk7/7.1.1%2Bdfsg2-10.2/
+
+wget https://sources.debian.org/data/main/v/vtk7/7.1.1%2Bdfsg2-10.2/debian/patches/10_allpatches.patch
+wget https://sources.debian.org/data/main/v/vtk7/7.1.1%2Bdfsg2-10.2/debian/patches/20_soversion-sharedlib.patch
+wget https://sources.debian.org/data/main/v/vtk7/7.1.1%2Bdfsg2-10.2/debian/patches/30_matplotlib.patch
+wget https://sources.debian.org/data/main/v/vtk7/7.1.1%2Bdfsg2-10.2/debian/patches/40_use_system_sqlite.patch
+wget https://sources.debian.org/data/main/v/vtk7/7.1.1%2Bdfsg2-10.2/debian/patches/50_use_system_utf8.patch
+wget https://sources.debian.org/data/main/v/vtk7/7.1.1%2Bdfsg2-10.2/debian/patches/60_use_system_mpi4py.patch
+wget https://sources.debian.org/data/main/v/vtk7/7.1.1%2Bdfsg2-10.2/debian/patches/70_fix_ftbfs_gcc49.patch
+wget https://sources.debian.org/data/main/v/vtk7/7.1.1%2Bdfsg2-10.2/debian/patches/80_fix_arm_compilation.patch
+wget https://sources.debian.org/data/main/v/vtk7/7.1.1%2Bdfsg2-10.2/debian/patches/100_javac-heap.patch
+wget https://sources.debian.org/data/main/v/vtk7/7.1.1%2Bdfsg2-10.2/debian/patches/101_java_install_path.patch
+wget https://sources.debian.org/data/main/v/vtk7/7.1.1%2Bdfsg2-10.2/debian/patches/105_unforce_embedded_glew.patch
+wget https://sources.debian.org/data/main/v/vtk7/7.1.1%2Bdfsg2-10.2/debian/patches/106_install_doxygen_scripts_in_nodoc_build.patch
+wget https://sources.debian.org/data/main/v/vtk7/7.1.1%2Bdfsg2-10.2/debian/patches/99-hdf5-1.10-compatibility
+wget https://sources.debian.org/data/main/v/vtk7/7.1.1%2Bdfsg2-10.2/debian/patches/107_gl2ps_1.4.0.patch
+wget https://sources.debian.org/data/main/v/vtk7/7.1.1%2Bdfsg2-10.2/debian/patches/108_Doxygen-use-mathjax.patch
+wget https://sources.debian.org/data/main/v/vtk7/7.1.1%2Bdfsg2-10.2/debian/patches/109_java-jar-nonjavafiles.patch
+wget https://sources.debian.org/data/main/v/vtk7/7.1.1%2Bdfsg2-10.2/debian/patches/110_python-371.patch
+wget https://sources.debian.org/data/main/v/vtk7/7.1.1%2Bdfsg2-10.2/debian/patches/111_fix_perl.patch
+wget https://sources.debian.org/data/main/v/vtk7/7.1.1%2Bdfsg2-10.2/debian/patches/112_riscv_support.patch
+wget https://sources.debian.org/data/main/v/vtk7/7.1.1%2Bdfsg2-10.2/debian/patches/113_fix_python_equal.patch
+wget https://sources.debian.org/data/main/v/vtk7/7.1.1%2Bdfsg2-10.2/debian/patches/115_support-gcc10.patch
+wget https://sources.debian.org/data/main/v/vtk7/7.1.1%2Bdfsg2-10.2/debian/patches/mysq8_my_bool.patch
+wget https://sources.debian.org/data/main/v/vtk7/7.1.1%2Bdfsg2-10.2/debian/patches/3edc0de2b04ae1e100c229e592d6b9fa94f2915a.patch
+wget https://sources.debian.org/data/main/v/vtk7/7.1.1%2Bdfsg2-10.2/debian/patches/581d9eb874b2b80a3fb21c739a96fa6f955ffb5e.patch
+wget https://sources.debian.org/data/main/v/vtk7/7.1.1%2Bdfsg2-10.2/debian/patches/gcc-11.patch
+wget https://sources.debian.org/data/main/v/vtk7/7.1.1%2Bdfsg2-10.2/debian/patches/ffmpeg-5.patch
+
+combinediff 10_allpatches.patch 20_soversion-sharedlib.patch > tmp1.patch
+combinediff tmp1.patch 30_matplotlib.patch > tmp2.patch
+combinediff tmp2.patch 40_use_system_sqlite.patch > tmp1.patch 
+combinediff tmp1.patch 60_use_system_mpi4py.patch > tmp2.patch
+combinediff tmp2.patch 70_fix_ftbfs_gcc49.patch > tmp1.patch
+combinediff tmp1.patch 80_fix_arm_compilation.patch > tmp2.patch
+combinediff tmp2.patch 100_javac-heap.patch > tmp1.patch 
+combinediff tmp1.patch 101_java_install_path.patch > tmp2.patch 
+combinediff tmp2.patch 105_unforce_embedded_glew.patch > tmp1.patch 
+combinediff tmp1.patch 106_install_doxygen_scripts_in_nodoc_build.patch > tmp2.patch 
+combinediff tmp2.patch 108_Doxygen-use-mathjax.patch > tmp1.patch 
+combinediff tmp1.patch 109_java-jar-nonjavafiles.patch > tmp2.patch 
+combinediff tmp2.patch 110_python-371.patch > tmp1.patch 
+combinediff tmp1.patch 111_fix_perl.patch > tmp2.patch 
+combinediff tmp2.patch 112_riscv_support.patch > tmp1.patch 
+combinediff tmp1.patch 113_fix_python_equal.patch > tmp2.patch 
+combinediff tmp2.patch 115_support-gcc10.patch > tmp1.patch 
+combinediff tmp1.patch mysq8_my_bool.patch > tmp2.patch 
+combinediff tmp2.patch 3edc0de2b04ae1e100c229e592d6b9fa94f2915a.patch > tmp1.patch 
+combinediff tmp1.patch 581d9eb874b2b80a3fb21c739a96fa6f955ffb5e.patch > tmp2.patch 
+combinediff tmp2.patch gcc-11.patch > tmp1.patch 
+combinediff tmp1.patch ffmpeg-5.patch > vtk7.patch 
+
+rm tmp1.patch tmp2.patch
+
