00994f5b786816a22eab6b4eb0c17f385a3b4c3c
Kwabena N Amponsah <kwabenantim@yahoo.com>
2023-03-17 23:05:48 +0000

    Fix boost pthread patch versions

--- scripts/install_boost.sh
+++ scripts/install_boost.sh
@@ -82,11 +82,11 @@ if [[ (${major} -eq 1) && (${minor} -le 64) ]]; then  # Boost <= 1.64.x
     patch -t -p2 < ${script_dir}/patches/boost_164-serialization-array-wrapper.patch
 fi
 
-# Patch for pthread in 1.69.x <= Boost <= 1.73.x
+# Patch for pthread in 1.69.x <= Boost <= 1.72.x
 # https://github.com/boostorg/thread/pull/297/commits/74fb0a26099bc51d717f5f154b37231ce7df3e98
-if [[ (${major} -eq 1) && (${minor} -ge 69) && (${minor} -le 73) ]]; then  # 1.69.x <= Boost <= 1.73.x
+if [[ (${major} -eq 1) && (${minor} -ge 69) && (${minor} -le 72) ]]; then  # 1.69.x <= Boost <= 1.72.x
     cd ${src_dir}
-    patch -t -p2 < ${script_dir}/patches/boost_173-pthread.patch
+    patch -t -p2 < ${script_dir}/patches/boost_169-pthread.patch
 fi
 
 # Build and install
