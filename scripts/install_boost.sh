e8b9a4f217d59c18b8e611ae0db9de84b9614815
Kwabena N Amponsah <kwabenantim@yahoo.com>
2023-12-07 15:15:45 +0000

    Fix boost system modulefile

--- scripts/install_boost.sh
+++ scripts/install_boost.sh
@@ -65,6 +65,8 @@ proc ModulesHelp { } {
 module-whatis "This adds the environment variables for boost ${version}"
 
 setenv          BOOST_ROOT           /usr
+setenv          BOOST_INCLUDEDIR     /usr/include/boost
+setenv          BOOST_LIBRARYDIR     /usr/lib/x86_64-linux-gnu
 
 prepend-path    LIBRARY_PATH         /usr/lib/x86_64-linux-gnu
 prepend-path    LD_LIBRARY_PATH      /usr/lib/x86_64-linux-gnu
