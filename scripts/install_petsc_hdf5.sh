342dbbb5315d3d80e89f55a9a1f3a416595621a6
Kwabena N Amponsah <kwabenantim@yahoo.com>
2023-12-08 13:40:20 +0000

    Add petsc-hdf5 system modulefile

--- scripts/install_petsc_hdf5.sh
+++ scripts/install_petsc_hdf5.sh
@@ -53,6 +53,78 @@ if [[ ! (${petsc_arch} = 'linux-gnu'
     usage
 fi
 
+# Modulefile pointing to system version
+if [[ ("$petsc_version" = "system") || ("$hdf5_version" = "system") || ("$petsc_arch" = "system") ]]; then
+petsc_version=$(dpkg -s libpetsc-real-dev | grep 'Version:' | cut -d' ' -f2 | cut -d. -f1,2,3 | cut -d+ -f1)
+hdf5_version=$(dpkg -s libhdf5-openmpi-dev | grep 'Version:' | cut -d' ' -f2 | cut -d. -f1,2,3 | cut -d+ -f1)
+
+mkdir -p ${base_dir}/modulefiles/petsc_hdf5/${petsc_version}_${hdf5_version}
+cd  ${base_dir}/modulefiles/petsc_hdf5/${petsc_version}_${hdf5_version}
+cat <<EOF > ${petsc_arch}
+#%Module1.0#####################################################################
+###
+## petsc_hdf5 ${petsc_version}_${hdf5_version}/${petsc_arch} modulefile
+##
+proc ModulesTest { } {
+    set paths "[getenv PETSC_DIR]
+               [getenv PETSC_DIR]/[getenv PETSC_ARCH]
+               [getenv PETSC_DIR]/[getenv PETSC_ARCH]/bin
+               [getenv PETSC_DIR]/[getenv PETSC_ARCH]/bin/h5pcc
+               [getenv PETSC_DIR]/[getenv PETSC_ARCH]/include
+               [getenv PETSC_DIR]/[getenv PETSC_ARCH]/lib
+               [getenv PETSC_DIR]/[getenv PETSC_ARCH]/lib/libhdf5.so
+               [getenv PETSC_DIR]/[getenv PETSC_ARCH]/lib/libpetsc.so"
+
+    foreach path \$paths {
+        if { ![file exists \$path] } {
+            puts stderr "ERROR: Does not exist: \$path"
+            return 0
+        }
+    }
+    return 1
+}
+
+proc ModulesHelp { } {
+    puts stderr "\tThis adds the environment variables for petsc ${petsc_version} and hdf5 ${hdf5_version}, with PETSC_ARCH=${petsc_arch}\n"
+}
+
+module-whatis "This adds the environment variables for petsc ${petsc_version} and hdf5 ${hdf5_version}, with PETSC_ARCH=${petsc_arch}"
+
+setenv          PETSC_DIR            ""
+setenv          PETSC_ARCH           ""
+
+prepend-path    PATH                 /usr/bin
+
+prepend-path    LIBRARY_PATH         /usr/lib/x86_64-linux-gnu
+prepend-path    LD_LIBRARY_PATH      /usr/lib/x86_64-linux-gnu
+prepend-path    LD_RUN_PATH          /usr/lib/x86_64-linux-gnu
+
+prepend-path    INCLUDE              /usr/include/petsc
+prepend-path    C_INCLUDE_PATH       /usr/include/petsc
+prepend-path    CPLUS_INCLUDE_PATH   /usr/include/petsc
+
+prepend-path    CMAKE_PREFIX_PATH    /usr/
+
+setenv          HDF5_ROOT            /usr/
+
+prepend-path    INCLUDE              /usr/include/hdf5
+prepend-path    C_INCLUDE_PATH       /usr/include/hdf5
+prepend-path    CPLUS_INCLUDE_PATH   /usr/include/hdf5
+
+setenv          PARMETIS_ROOT        /usr/
+
+prepend-path    INCLUDE              /usr/include
+prepend-path    C_INCLUDE_PATH       /usr/include
+prepend-path    CPLUS_INCLUDE_PATH   /usr/include
+
+conflict petsc
+conflict hdf5
+conflict petsc_hdf5
+EOF
+
+exit 0
+fi
+
 petsc_version_arr=(${petsc_version//\./ })
 petsc_major=${petsc_version_arr[0]}
 petsc_minor=${petsc_version_arr[1]}
