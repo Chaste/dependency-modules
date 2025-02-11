5f62998ce67394ae97bf39193b133cdcdeddb8b4
Kwabena N Amponsah <kwabenantim@yahoo.com>
2025-02-11 01:02:55 +0000

    #76 Simplify system setup scripts

--- scripts/system/setup_ubuntu.sh
+++ scripts/system/setup_ubuntu.sh
@@ -2,6 +2,8 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
+codename="$(. /etc/os-release && echo ${VERSION_CODENAME} | sed 's/\.//')"
+
 # System dependencies
 apt-get update && \
 apt-get install -y --no-install-recommends \
@@ -17,43 +19,20 @@ apt-get install -y --no-install-recommends \
   wget
 
 # Chaste dependencies
+echo "deb [signed-by=/usr/share/keyrings/chaste.asc] https://chaste.github.io/ubuntu ${codename}/" > /etc/apt/sources.list.d/chaste.list
+wget -O /usr/share/keyrings/chaste.asc https://chaste.github.io/chaste.asc
+
 apt-get update && \
 apt-get install -y --no-install-recommends \
-  cmake \
-  cmake-curses-gui \
+  chaste-dependencies \
   doxygen \
-  g++ \
   git \
-  gnuplot \
-  graphviz \
-  hdf5-tools \
   lcov \
-  libboost-serialization-dev \
-  libboost-filesystem-dev \
-  libboost-program-options-dev \
-  libhdf5-openmpi-dev \
-  libmetis-dev \
-  libopenmpi-dev \
-  libparmetis-dev \
-  libpetsc-real3.15 \
-  libpetsc-real3.15-dbg \
-  libpetsc-real3.15-dev \
-  libsundials-dev \
-  libvtk9-dev \
-  libxerces-c-dev \
-  mencoder \
-  petsc-dev \
   python3 \
   python3-dev \
   python3-pip \
   python3-venv \
-  valgrind \
-  vtk9 \
-  xsdcxx
-
-# https://bugs.launchpad.net/ubuntu/+source/expat/+bug/2058415
-apt-get install libexpat1=2.4.7-1 libexpat1-dev=2.4.7-1
-apt-mark hold libexpat1 libexpat1-dev
+  valgrind
 
 update-alternatives --install /usr/local/bin/python python /usr/bin/python3 10
 update-alternatives --install /usr/local/bin/pip pip /usr/bin/pip3 10
