997019a57f3c6439dceb7ccd1157ce519a4b503a
Kwabena N Amponsah <kwabenantim@yahoo.com>
2025-02-11 08:03:53 +0000

    #76 Add libexpat fix for jammy

--- scripts/system/setup_ubuntu.sh
+++ scripts/system/setup_ubuntu.sh
@@ -2,8 +2,6 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
-codename="$(. /etc/os-release && echo ${VERSION_CODENAME} | sed 's/\.//')"
-
 # System dependencies
 apt-get update && \
 apt-get install -y --no-install-recommends \
@@ -19,6 +17,7 @@ apt-get install -y --no-install-recommends \
   wget
 
 # Chaste dependencies
+codename="$(. /etc/os-release && echo ${VERSION_CODENAME} | sed 's/\.//')"
 echo "deb [signed-by=/usr/share/keyrings/chaste.asc] https://chaste.github.io/ubuntu ${codename}/" > /etc/apt/sources.list.d/chaste.list
 wget -O /usr/share/keyrings/chaste.asc https://chaste.github.io/chaste.asc
 
@@ -33,6 +32,12 @@ apt-get install -y --no-install-recommends \
   python3-pip \
   python3-venv \
   valgrind
+
+# https://bugs.launchpad.net/ubuntu/+source/expat/+bug/2058415
+if [ "${codename}" = 'jammy' ]; then
+    apt-get install libexpat1=2.4.7-1 libexpat1-dev=2.4.7-1
+    apt-mark hold libexpat1 libexpat1-dev
+fi
 
 update-alternatives --install /usr/local/bin/python python /usr/bin/python3 10
 update-alternatives --install /usr/local/bin/pip pip /usr/bin/pip3 10
